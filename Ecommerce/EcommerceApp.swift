7cbaa2ed0b55efa619b8f1ac56bf1116814b1c50
MubashirPM <mubashirpmmubashir@gmail.com>
2026-02-09 21:27:55 +0530

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Mubashir PM on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
